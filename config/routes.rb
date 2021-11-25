161c8daaedc11c1136cee45533d5e4073120aa3d
Ubuntu <ubuntu@ip-172-31-9-76.us-east-2.compute.internal>
2021-11-25 17:23:23 +0000

    newのルーティングを確認

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'lessons/hello'
-  get 'users/index'
+  get 'users/index', to: 'users#index'
+  
   get 'lessons/call'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
